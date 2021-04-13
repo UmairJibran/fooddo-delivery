754ea504ed91c92b5c07fa8c3a9bd8d37261fbf8
Umair Jibran <umairjibran7@gmail.com>
2021-04-14 00:36:06 +0500

    add firebase config

---
+++ lib/classes/delivery_person.dart
@@ -0,0 +1,8 @@
+class DeliveryPerson {
+  final String name;
+  final String city;
+  final String phone;
+  final int vehicleCapacity;
+
+  DeliveryPerson(this.name, this.city, this.phone, this.vehicleCapacity);
+}
